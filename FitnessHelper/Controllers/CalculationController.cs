a199708cd6ac58306905b71003ee38a0cf9b8489
viniraf <vrmb.pro@gmail.com>
2024-06-25 21:16:48 -0300

    Added authorize atribute to calculation group endpoints

--- FitnessHelper/Controllers/CalculationController.cs
+++ FitnessHelper/Controllers/CalculationController.cs
@@ -1,6 +1,7 @@
 ﻿using FitnessHelper.Enums;
 using FitnessHelper.Models;
 using FitnessHelper.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
@@ -21,6 +22,7 @@ public class CalculationController : ControllerBase
     }
 
     [HttpGet("basal-metabolic-rate")]
+    [Authorize]
     public IActionResult BasalMetabolicRate([FromQuery] Sex sex, double weight, double height, int age, [FromQuery] ExerciseTimesPerWeek exerciseTimesPerWeek)
     {
         int roundedBasalMetabolicRate = _calculationService.CalculateBasalMetabolicRate(sex, weight, height, age, exerciseTimesPerWeek);
@@ -28,6 +30,7 @@ public class CalculationController : ControllerBase
     }
 
     [HttpGet("macronutrient")]
+    [Authorize]
     public IActionResult Macronutrient([FromQuery] int basalMetabolicRate, [FromQuery] double weight, [FromQuery] Goal goal)
     {
         MacronutrientCalculationModel macronutrientCalculationModel = _calculationService.CalculateMacronutrient(basalMetabolicRate, weight, goal);
