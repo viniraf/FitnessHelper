b593d3ff0baa12d6e7fcf562ff9ca0c7e79bb6d1
viniraf <vrmb.pro@gmail.com>
2024-07-02 12:23:26 -0300

    Created TrainingHistory table

---
+++ FitnessHelper/Controllers/HistoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TGolla.Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace FitnessHelper.Controllers
+{
+    [Route("api/history")]
+    [SwaggerControllerOrder(7)]
+    [ApiController]
+    public class HistoryController : ControllerBase
+    {
+        [HttpPost("/training")]
+        public IActionResult PostTrainingHistory()
+        {
+            return Ok();
+        }
+
+        [HttpPost("/weight")]
+        public IActionResult PostWeightHistory()
+        {
+            return Ok();
+        }
+
+        [HttpGet("/training")]
+        public IActionResult GetTrainingHistory()
+        {
+            return Ok();
+        }
+
+
+        [HttpGet("/weight")]
+        public IActionResult GetWeightHistory()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ FitnessHelper/Models/TrainingHistoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FitnessHelper.Models
+{
+    public class TrainingHistoryModel
+    {
+        public int Id { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public int TrainingId { get; set; }
+
+        [JsonIgnore]
+        public TrainingModel Training { get; set; }
+    }
+}
